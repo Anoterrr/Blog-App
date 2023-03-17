9509b93316ba4cfa25e0fe6c4abf062c649043c0
Anoter <prosidegcr@gmail.com>
2023-03-17 20:49:52 -0300

    Commenting Script imported

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 import Head from 'next/head';
-import Script from 'next/script';
+// import Script from 'next/script';
 
 export default function FirstPost() {
   return (
